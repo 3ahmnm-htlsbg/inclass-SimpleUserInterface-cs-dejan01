93c0ff52a0ba665aaf3d0df29a4238a1b943541f
Dejan01 <dejan.klickovic01@htl-salzburg.ac.at>
2020-01-07 14:44:16 +0100

    create C# Script, attach to EmptyObject and declare Variables

---
+++ Assets/MyGame/Script/SimpleUserInterface.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleUserInterface : MonoBehaviour
+{
+    public Text Name_result;
+    public Text Number_varB;
+    public Button Button_name;
+    public Button ButtonNumber;
+    public InputField InputField_name;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
